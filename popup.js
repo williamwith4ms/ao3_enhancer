affc70bab23bfb35677747193bb76daf97fa0d8e
williammmm <me@williammmm.dev>
2026-07-03 18:49:52 +0100

    feat: add popup showing read works and word count

---
+++ popup.js
@@ -0,0 +1,16 @@
+(async function() {
+    const storedWorks = await browser.storage.local.get(null);
+
+    let totalWordCount = 0;
+    let totalWorksRead = 0;
+
+    const readWorks = Object.entries(storedWorks).filter(([, workData]) => workData && workData.read);
+
+    for (const [workId, workData] of readWorks) {
+        totalWordCount += workData.wordCount || 0;
+        totalWorksRead++;
+    }
+
+    document.getElementById("total-works-read").textContent = totalWorksRead;
+    document.getElementById("total-word-count").textContent = totalWordCount.toLocaleString();
+})();
